4cbeea2201f7148c1c0f8e032732c8e4bd555d6a
Mohammadreza Fasli <mohammadrezafasli1234@gmail.com>
2026-02-13 13:32:37 +0330

    Initial commit

---
+++ contacts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200 , default="unknown")
+    created_date = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-created_date']
+        
+    
