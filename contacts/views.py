a30a1a9ad7b355b14e01c95936e99d75a1a08948
Mohammadreza Fasli <mohammadrezafasli1234@gmail.com>
2026-02-13 14:56:51 +0330

    make clear views.py

--- contacts/views.py
+++ contacts/views.py
@@ -1,19 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Contact
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators import login_required
 from .models import Contact
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the contacts index.")
 
+
 @login_required
 def contact_list(request):
     contacts = Contact.objects.filter(user=request.user)
     return render(request, "contacts/contact_list.html", {"contacts": contacts})
+
 
 @login_required
 def contact_detail(request, id):
